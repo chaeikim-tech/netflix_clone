531e515687a1199f00f6c6d3cf5841ce138490d6
eunju.kim <sb02348@hanmail.net>
2022-09-25 02:23:38 +0900

    Add framer-motion props Animation

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ const Wrapper = styled.div`
   height: 100vh;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
     background-color: white;
-    border-radius: 10px;
+    border-radius: 15px;
     box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px rgba(0,0,0,0.06);
     `;
 
@@ -22,8 +22,11 @@ function App() {
 
     return (
         <Wrapper>
-            <Box />
-            <motion.div></motion.div>
+            <Box
+                transition={{ type: "spring", delay: 0.5 }}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1, rotateZ: 360 }}
+            />
         </Wrapper>
     );
 }
